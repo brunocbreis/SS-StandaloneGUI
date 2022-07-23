467362c0872b7ea2ae6d8fb79a6fb4818ee6cee4
Bruno Reis <brunocbreis@gmail.com>
2022-07-23 17:07:02 -0300

    terminal version working!

--- ss_terminal.py
+++ ss_terminal.py
@@ -1,12 +1,15 @@
 from ss_classes import Canvas, Margin, Grid, Screen
 import ss_user_input_functions as user
+from ss_generator import render_fusion_output, save_preset
 from pickle import load
 from os import listdir
 from os.path import join
+from time import sleep
 
 
 # Load defaults.
-print('Welcome to SplitScreener. Loading defaults...\n')
+print('\nWelcome to SplitScreener. Press [ENTER] start.')
+input("")
 
 defaults_files = listdir('defaults')
 defaults_files.sort()
@@ -31,14 +34,80 @@ margin.top, margin.left, margin.bottom, margin.right = [margin_defaults[key] for
 grid = Grid(canvas, margin)
 grid.cols, grid.rows, grid.gutter = [grid_defaults[key] for key in [*grid_defaults]]
 
+print("DEFAULT SETTINGS")
 print(canvas,margin,grid, sep='\n')
 
 
 # User customizes setup.
-
 wants_to_customize = user.ask_if_custom_setup() 
+using_defaults = not wants_to_customize
+
 while wants_to_customize:
     user_choice = user.choose_custom_setup()
-    print(f'You want to change the {user_choice.title()}.')
+    print(f'Enter your custom {user_choice.title()} settings below.')
+    if user_choice == 'canvas':
+        custom_values = user.customize_canvas()
+        canvas.width, canvas.height = [value for value in custom_values]
+
+    if user_choice == 'margin':
+        custom_values = user.customize_margin()
+        margin.top, margin.left, margin.bottom, margin.right = [value for value in custom_values]
+    
+    if user_choice == 'grid':
+        custom_values = user.customize_grid()
+        grid.cols, grid.rows, grid.gutter = [value for value in custom_values]
+    
     wants_to_customize = user.ask_if_custom_setup(False)
 
+if not using_defaults:
+    print(canvas,margin,grid, sep='\n')
+
+input("Press [ENTER] to continue.")
+
+
+# User creates screens.
+screens = []
+
+print("\nTime to set up your SplitScreener Screens!")
+print("For each new Screen, you'll be asked to provide 4 values. [ENTER]")
+input("")
+print("WIDTH (measured in columns), HEIGHT (measured in rows),")
+print(f"X POSITION (1 is the leftmost column, {grid.cols} is the rightmost column),")
+print(f"and Y POSITION (1 being the top row, {grid.rows} being the bottom row.\n")
+sleep(3)
+
+input("When you're ready, press [ENTER].")
+
+wants_more_screens = True
+while wants_more_screens:
+    print(f"\nSCREEN {len(screens) + 1}")
+    cv = user.screen_config()
+    screen = Screen(
+        grid = grid,
+        colspan=cv[0],
+        rowspan=cv[1],
+        colx=cv[2],
+        coly=cv[3]
+        )
+    screens.append(screen)
+    wants_more_screens = user.ask_if_more_screens()
+
+input(f"You have created {len(screens)} Screen{'s' if len(screens) > 1 else ''}. [ENTER]")
+print("Computing SplitScreener values...")
+
+screen_values = []
+for screen in screens:
+    screen_value = screen.get_values()
+    screen_values.append(screen_value)
+
+sleep(1)
+
+# SplitScreener renders Fusion output
+input("Press [ENTER] to render Fusion output.")
+
+fusion_output = render_fusion_output(canvas, screen_values)
+
+want_to_save = input("Choose a name for your new preset. Or press [ENTER] to leave without saving.\n")
+
+if want_to_save:
+    save_preset('presets',fusion_output,want_to_save)
--- ss_user_input_functions.py
+++ ss_user_input_functions.py
@@ -1,6 +1,7 @@
+# Setup input functions
 def ask_if_custom_setup(first_time: bool = True) -> bool:
     """Asks user if they want to customize the setup."""
-    
+
     thing = 'your setup'
     adjective = 'the default'
     verb = 'Stick'
@@ -11,17 +12,16 @@ def ask_if_custom_setup(first_time: bool = True) -> bool:
 
     while True:
         wants_to_customize = input(f'Do you want to customize {thing}? (yes / no)\n> ').lower()
-        if wants_to_customize == 'yes':
+        if wants_to_customize in ['yes','y']:
             wants_to_customize = True
             break
-        if wants_to_customize == 'no':
+        if wants_to_customize in ['no', 'n']:
             wants_to_customize = False
-            print(f'Ok. {verb}ing with {adjective} settings.\n')
+            print(f'Ok. {verb}ing with {adjective} settings.')
             break
         print("I'm sorry, I don't understand. Let's try again:")
 
     return wants_to_customize
-
 
 def choose_custom_setup() -> str:
     while True:
@@ -38,12 +38,70 @@ def choose_custom_setup() -> str:
         print("I'm not sure I understand. Let's try again:")
     return user_choice
 
+def customize_canvas() -> list[int]:
+    attributes = ['Width','Height']
+    custom_values = []
+    for attribute in attributes:
+        user_choice = input(f"{attribute}: ")
+        while not user_choice.isnumeric():
+            print('Please only type in numbers.')
+            user_choice = input(f"{attribute}: ")
+        custom_values.append(int(user_choice))
+    return custom_values
 
-def customize_canvas():
+def customize_margin() -> list[int]:
+    attributes = ['Top', 'Left', 'Bottom', 'Right']
+    custom_values = []
+    for attribute in attributes:
+        user_choice = input(f"{attribute}: ")
+        while not user_choice.isnumeric():
+            print('Please only type numbers.')
+            user_choice = input(f"{attribute}: ")
+        custom_values.append(int(user_choice))
+    return custom_values
+
+def customize_grid() -> list[int]:
+    attributes = ['Cols', 'Rows', 'Gutter (px)']
+    custom_values = []
+    for attribute in attributes:
+        user_choice = input(f"{attribute}: ")
+        while not user_choice.isnumeric():
+            print('Please only type numbers.')
+            user_choice = input(f"{attribute}: ")
+        custom_values.append(int(user_choice))
+    return custom_values
+
+
+# Screen input functions
+def ask_if_more_screens() -> bool:
+    while True:
+        wants_more_screens = input('Do you want to create another Screen? (yes / no)\n> ').lower()
+        if wants_more_screens in ['yes', 'y']:
+            wants_more_screens = True
+            break
+        if wants_more_screens in ['no', 'n']:
+            wants_more_screens = False
+            break
+        print("Come on. It's a yes or no question.")
+
+    return wants_more_screens
+
+def screen_config() -> list[int]:
+    attributes = [
+        'Width (cols)',
+        'Height (rows)',
+        'X Position (leftmost col #)',
+        'Y Position (top row #)'
+        ]
+    custom_values = []
+    for attribute in attributes:
+        user_choice = input(f"{attribute}: ")
+        while not user_choice.isnumeric():
+            print('Numbers only please.')
+            user_choice = input(f"{attribute}: ")
+        custom_values.append(int(user_choice))
+    return custom_values
+
+def screen_config_coords() -> list[int]:
     pass
 
-def customize_margin():
-    pass
-
-def customize_grid():
-    pass
